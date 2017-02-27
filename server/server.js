6f536a41650537a6af76a143a2dae9723e26a885
Hervinho <herventubi@gmail.com>
2017-02-27 14:50:14 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb'); //get ObjectID from mongodb library
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 
 const port = /*process.env.PORT ||*/ 3000;
 
+//create todo
 app.post('/todos', (req, res) => {
   //Create Todo model.
   var todo = new Todo({
@@ -25,14 +27,32 @@ app.post('/todos', (req, res) => {
 
 });
 
+//get all todos
 app.get('/todos', (req, res) => {
-  //get all todos
   Todo.find().then((todos) => {
     res.send({todos});
   }, (error) => {
     res.status(400).send(error);
   })
 
+});
+
+//get a single todo
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if( !ObjectID.isValid(id) ){
+    res.status(400).send({response: 'Id not valid'});
+  }
+  else{
+    Todo.findById(id).then((todo) => {
+      if(!todo){
+        res.status(404).send({response: 'Todo not found'});
+      }
+      res.status(200).send({todo});
+    }, (error) => {
+      res.status(400).send(error);
+    });
+  }
 });
 
 app.listen(port, () => {

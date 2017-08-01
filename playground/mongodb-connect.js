e3821b8dd3ccaf5981808e5b09155440994adc88
Hervinho <herventubi@gmail.com>
2017-08-01 11:57:12 +0200

    equality matches on arry

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,29 +7,60 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-  // db.collection('Todos').insertOne({
-  //   text: 'Something to do',
-  //   completed: false
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log('Unable to insert todo', err);
-  //   }
-  //
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // });
+  //find all
+  db.collection('Todos').find({
+     //_id: new ObjectID('57bb36afb3b6a3801d8c479d')
+     //text: 'hiii', completed: false
+     //'details.time': '10.00 am'
+     //writers: ["You", "Jane Doe", "Aliens"]//exact match in the array
+     //writers: "Jane Doe" //anything that matches (even partially)
+     "writers.0": "You" //only matching with first element of the array
+  }).toArray().then((docs) => {
+     console.log('Todos');
+     console.log(JSON.stringify(docs, undefined, 2));
+   }, (err) => {
+     console.log('Unable to fetch todos', err);
+  });
 
-  // Insert new doc into Users (name, age, location)
-  // db.collection('Users').insertOne({
-  //   name: 'Andrew',
-  //   age: 25,
-  //   location: 'Philadelphia'
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log('Unable to insert user', err);
-  //   }
-  //
-  //   console.log(result.ops[0]._id.getTimestamp());
-  // });
+  //find all and count.
+  /*db.collection('Todos').find().count().then((count) => {
+     console.log(`Todos count: ${count}`);
+   }, (err) => {
+     console.log('Unable to fetch todos', err);
+  });*/
+
+  //insert one. 
+  /*db.collection('Todos').insertOne({
+    text: 'play',
+    completed: true,
+    //details: {date: '22/09/1993', time: '11.00 pm'},
+    writers: ["You", "Jane Doe", "Aliens"]
+   }, (err, result) => {
+     if (err) {
+      return console.log('Unable to insert todo', err);
+    }
+  
+     console.log(JSON.stringify(result.ops, undefined, 2));
+  });*/
+
+
+  //Insert many
+  /*db.collection('Todos').insertMany([
+    {
+     text: 'hello wolr',
+     completed: false
+    },
+    {
+     text: 'hiii',
+     completed: false
+    }
+  ], (err, result) => {
+     if (err) {
+       return console.log('Unable to insert todo', err);
+    }
+  
+     console.log('Inserted');
+   });*/
 
   db.close();
 });
